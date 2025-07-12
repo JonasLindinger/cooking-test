fafbc2fcfa97faf530ba0864339819e73dca2321
Jonas <lindinger.jonas.yt@gmail.com>
2025-07-13 00:20:27 +0200

    add: tomato spawning when interacting

--- Assets/_Project/Scripts/Counters/ClearCounter.cs
+++ Assets/_Project/Scripts/Counters/ClearCounter.cs
@@ -4,9 +4,13 @@ namespace _Project.Scripts.Counters
 {
     public class ClearCounter : MonoBehaviour
     {
+        [SerializeField] private GameObject tomatoPrefab;
+        [SerializeField] private Transform counterTopPoint;
+        
         public void Interact()
         {
-            Debug.Log("Interact");
+            Transform tomatoTransform = Instantiate(tomatoPrefab, counterTopPoint).transform;
+            tomatoTransform.localPosition = Vector3.zero;
         }
     }
 }
