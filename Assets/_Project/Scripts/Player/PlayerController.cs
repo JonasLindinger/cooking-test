bf6994cf4602c4604e58a8ae1401354c5d8c8222
Jonas <lindinger.jonas.yt@gmail.com>
2025-07-12 23:09:46 +0200

    add: Improved collision system

--- Assets/_Project/Scripts/Player/PlayerController.cs
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -21,12 +21,48 @@ namespace Project.Player
             
             Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
 
-            float playerSize = .7f;
-            bool canMove = !Physics.Raycast(transform.position, moveDirection, playerSize);
+            float moveDistance = moveSpeed * Time.deltaTime;
+            
+            float playerRadius = .5f;
+            float playerHeight = 2f;
+            bool canMove = !Physics.CapsuleCast(transform.position, 
+                transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
 
+            // Split down movement into X and Z, if XZ Movement is blocked
+            if (!canMove)
+            {
+                // Cannot move towards moveDirection
+                
+                // Attempt only X movement
+                Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0);
+                canMove = !Physics.CapsuleCast(transform.position, 
+                    transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
+
+                if (canMove)
+                {
+                    // Can move only on the X
+                    moveDirection = moveDirectionX;
+                }
+                else
+                {
+                    // Cannot move only on the X
+                    
+                    // Attempt only X movement
+                    Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z);
+                    canMove = !Physics.CapsuleCast(transform.position, 
+                        transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
+
+                    if (canMove)
+                    {
+                        // Can move only on the Z
+                        moveDirection = moveDirectionZ;
+                    }
+                }
+            }
+            
             if (canMove)
             {
-                transform.position += moveDirection * moveSpeed * Time.deltaTime;
+                transform.position += moveDirection * moveDistance;
             }
             
             isWalking = moveDirection != Vector3.zero;
