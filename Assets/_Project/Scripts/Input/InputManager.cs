45e98054c74cf2588328503b64de9ef59319ca4f
Jonas <lindinger.jonas.yt@gmail.com>
2025-07-12 23:41:38 +0200

    add: Interaction Event

--- Assets/_Project/Scripts/Input/InputManager.cs
+++ Assets/_Project/Scripts/Input/InputManager.cs
@@ -1,17 +1,28 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace _Project.Scripts.Input
 {
     public class InputManager : MonoBehaviour
     {
+        public event EventHandler OnInteractAction; 
+        
         private PlayerInputActions playerInputActions;
 
         private void Awake()
         {
             playerInputActions = new PlayerInputActions();
             playerInputActions.Player.Enable();
+
+            playerInputActions.Player.Interact.performed += InteractPerformed;
         }
 
+        private void InteractPerformed(InputAction.CallbackContext context)
+        {
+            OnInteractAction?.Invoke(this, EventArgs.Empty);
+        }
+        
         public Vector2 GetNormalizedMovementVector()
         {
             Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
