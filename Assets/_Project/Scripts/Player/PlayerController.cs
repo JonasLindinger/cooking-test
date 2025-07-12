eff6dd3016b54596e997826affb22b1f97d974a9
Jonas <lindinger.jonas.yt@gmail.com>
2025-07-12 22:27:09 +0200

    add: Player Movement

--- Assets/_Project/Scripts/Player/PlayerController.cs
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -4,6 +4,34 @@ namespace Project.Player
 {
     public class PlayerController : MonoBehaviour
     {
-        
+        [Header("Movement")]
+        [SerializeField] private float moveSpeed = 7f;
+
+        private void Update()
+        {
+            Vector2 inputVector = Vector2.zero;
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                inputVector.y += 1;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                inputVector.y -= 1;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                inputVector.x -= 1;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                inputVector.x += 1;
+            }
+
+            inputVector = inputVector.normalized;
+            
+            Vector3 moveDirections = new Vector3(inputVector.x, 0, inputVector.y);
+            transform.position += moveDirections * moveSpeed * Time.deltaTime;
+        }
     }   
 }
