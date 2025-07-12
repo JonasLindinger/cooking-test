f9872eac0cfaf560de21f72c9627f0e5af69ecd8
Jonas <lindinger.jonas.yt@gmail.com>
2025-07-13 00:32:29 +0200

    add: Kitchen Object Scriptable Object

--- Assets/_Project/Scripts/Counters/ClearCounter.cs
+++ Assets/_Project/Scripts/Counters/ClearCounter.cs
@@ -1,16 +1,17 @@
-﻿using UnityEngine;
+﻿using _Project.Scripts.Object;
+using UnityEngine;
 
 namespace _Project.Scripts.Counters
 {
     public class ClearCounter : MonoBehaviour
     {
-        [SerializeField] private GameObject tomatoPrefab;
+        [SerializeField] private KitchenObject tomatoPrefab;
         [SerializeField] private Transform counterTopPoint;
         
         public void Interact()
         {
-            Transform tomatoTransform = Instantiate(tomatoPrefab, counterTopPoint).transform;
-            tomatoTransform.localPosition = Vector3.zero;
+            Transform kitchenObjectTransform = Instantiate(tomatoPrefab.prefab, counterTopPoint).transform;
+            kitchenObjectTransform.localPosition = Vector3.zero;
         }
     }
 }
---
+++ Assets/_Project/Scripts/ScriptableObjects/KitchenObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Object
+{
+    [CreateAssetMenu()]
+    public class KitchenObject : ScriptableObject
+    {
+        public string objectName;
+        public Sprite sprite;
+        public GameObject prefab;
+    }
+}
