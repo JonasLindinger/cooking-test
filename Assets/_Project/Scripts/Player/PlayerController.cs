3f184146fedcfb3a827f41db2a1760174aeb82a5
Jonas <lindinger.jonas.yt@gmail.com>
2025-07-12 22:53:16 +0200

    add: Getter IsWalking

--- Assets/_Project/Scripts/Player/PlayerController.cs
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -5,17 +5,24 @@ namespace Project.Player
 {
     public class PlayerController : MonoBehaviour
     {
+        // Getters
+        public bool IsWalking => isWalking;
+        
         [Header("Movement")]
         [SerializeField] private float moveSpeed = 7f;
         
         [SerializeField] private InputManager inputManager;
 
+        private bool isWalking;
+        
         private void Update()
         {
             Vector2 inputVector = inputManager.GetNormalizedMovementVector();
             
             Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            
+            isWalking = moveDirection != Vector3.zero;
             
             float rotationSpeed = 10f;
             transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
