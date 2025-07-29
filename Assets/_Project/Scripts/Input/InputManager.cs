59d5a65bb5616e6697fbab3bdcbd9346ab4b7d42
Jonas <lindinger.jonas.yt@gmail.com>
2025-07-29 12:20:10 +0200

    add: new input

--- Assets/_Project/Scripts/Input/InputManager.cs
+++ Assets/_Project/Scripts/Input/InputManager.cs
@@ -7,6 +7,7 @@ namespace _Project.Scripts.Input
     public class InputManager : MonoBehaviour
     {
         public event EventHandler OnInteractAction; 
+        public event EventHandler OnInteractAlternateAction; 
         
         private PlayerInputActions playerInputActions;
 
@@ -16,11 +17,17 @@ namespace _Project.Scripts.Input
             playerInputActions.Player.Enable();
 
             playerInputActions.Player.Interact.performed += InteractPerformed;
+            playerInputActions.Player.InteractAlternate.performed += InteractAlternatePerformed;
         }
 
         private void InteractPerformed(InputAction.CallbackContext context)
         {
             OnInteractAction?.Invoke(this, EventArgs.Empty);
+        }
+        
+        private void InteractAlternatePerformed(InputAction.CallbackContext context)
+        {
+            OnInteractAlternateAction?.Invoke(this, EventArgs.Empty);
         }
         
         public Vector2 GetNormalizedMovementVector()
