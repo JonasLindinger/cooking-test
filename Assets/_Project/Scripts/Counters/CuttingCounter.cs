e914c0de700e6fce93760cadd95d5e3bdac94f44
Jonas <lindinger.jonas.yt@gmail.com>
2025-07-29 12:16:00 +0200

    add: cutting counter (just doing the same as a clear counter (for now))

---
+++ Assets/_Project/Scripts/Counters/CuttingCounter.cs
@@ -0,0 +1,37 @@
+﻿using Project.Player;
+
+namespace _Project.Scripts.Counters
+{
+    public class CuttingCounter : BaseCounter
+    {
+        public override void Interact(PlayerController player)
+        {
+            if (!HasKitchenObject())
+            {
+                // There is no KitchenObject here
+                if (player.HasKitchenObject())
+                {
+                    // Player is carrying something
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+                }
+                else
+                {
+                    // Player not carrying anything
+                }
+            }
+            else
+            {
+                // There is a KitchenObject here
+                if (player.HasKitchenObject())
+                {
+                    // Player is carrying something
+                }
+                else
+                {
+                    // Player not carrying anything
+                    GetKitchenObject().SetKitchenObjectParent(player);
+                }
+            }
+        }
+    }
+}
