186f870cb5748a36b1f27ebd60d29fbb8a83fbcc
Jonas <lindinger.jonas.yt@gmail.com>
2025-07-30 15:13:34 +0200

    add: Trash Counter

---
+++ Assets/_Project/Scripts/Counters/TrashCounter.cs
@@ -0,0 +1,13 @@
+﻿using Project.Player;
+
+namespace _Project.Scripts.Counters
+{
+    public class TrashCounter : BaseCounter
+    {
+        public override void Interact(PlayerController player)
+        {
+            if (player.HasKitchenObject())
+                player.GetKitchenObject().DestroySelf();
+        }   
+    }
+}
