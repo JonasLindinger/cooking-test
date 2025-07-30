5da58ec6752c7258848426be26b77bc7bc2d9a4d
Jonas <lindinger.jonas.yt@gmail.com>
2025-07-30 14:42:37 +0200

    add: LookAtCamera on Cutting Counter

---
+++ Assets/_Project/Scripts/Helper/LookAtCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Helper
+{
+    public class LookAtCamera : MonoBehaviour
+    {
+        private void LateUpdate()
+        {
+            transform.LookAt(Camera.main.transform);
+        }
+    }
+}
