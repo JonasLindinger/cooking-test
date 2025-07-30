5769379134218582b806ba282f70ff94d63e1aa0
Jonas <lindinger.jonas.yt@gmail.com>
2025-07-30 18:15:07 +0200

    add: Plate counter (Spawns plates)

---
+++ Assets/_Project/Scripts/Counters/PlatesCounter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using _Project.Scripts.Kitchen;
+using _Project.Scripts.Object;
+using Project.Player;
+using UnityEngine;
+
+namespace _Project.Scripts.Counters
+{
+    public class PlatesCounter : BaseCounter
+    {
+        public event EventHandler OnPlateSpawned;
+        public event EventHandler OnPlateRemoved;
+        
+        [Header("Settings")]
+        [SerializeField] private float spawnPlateTimerMax = 4;
+        [SerializeField] private int platesSpawnedAmountMax = 4;
+        [Space(10)]
+        [Header("References")]
+        [SerializeField] private KitchenScriptableObject plateKitchenObject;
+        
+        private float spawnPlateTimer;
+
+        private int platesSpawnedAmount;
+
+        private void Update()
+        {
+            spawnPlateTimer += Time.deltaTime;
+            
+            if (spawnPlateTimer > spawnPlateTimerMax)
+            {
+                spawnPlateTimer = 0;
+
+                if (platesSpawnedAmount < platesSpawnedAmountMax)
+                {
+                    platesSpawnedAmount++;
+                    
+                    OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        public override void Interact(PlayerController player)
+        {
+            if (!player.HasKitchenObject())
+            {
+                // Player is empty handed
+                if (platesSpawnedAmount > 0)
+                {
+                    // There's at least one plate here
+                    platesSpawnedAmount--;
+                    
+                    KitchenObject.SpawnKitchenObject(plateKitchenObject, player);
+                    
+                    OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Counters/PlatesCounterVisual.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Project.Scripts.Counters
+{
+    public class PlatesCounterVisual : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private PlatesCounter platesCounter;
+        [SerializeField] private Transform counterTopPoint;
+        [SerializeField] private GameObject plateVisualPrefab;
+        [Space(10)]
+        [SerializeField] private float plateOffsetY = 0.03f;
+        
+        private List<GameObject> plateVisuals = new List<GameObject>();
+
+        private void Start()
+        {
+            platesCounter.OnPlateSpawned += PlatesCounterOnOnPlateSpawned;
+            platesCounter.OnPlateRemoved += PlatesCounterOnOnPlateRemoved;
+        }
+
+        private void PlatesCounterOnOnPlateSpawned(object sender, EventArgs e)
+        {
+            Transform plateVisualTransform = Instantiate(plateVisualPrefab, counterTopPoint).transform;
+            
+            plateVisualTransform.localPosition = new Vector3(0, plateOffsetY * plateVisuals.Count, 0);
+            
+            plateVisuals.Add(plateVisualTransform.gameObject);
+        }
+        
+        private void PlatesCounterOnOnPlateRemoved(object sender, EventArgs e)
+        {
+            GameObject plateVisual = plateVisuals[plateVisuals.Count - 1];
+            plateVisuals.Remove(plateVisual);
+            
+            Destroy(plateVisual);
+        }
+    }
+}
