0e1fbb1bb933ac149f5104128c8f5560f0c6f093
Jonas <lindinger.jonas.yt@gmail.com>
2025-07-31 11:57:58 +0200

    add: Delivery counter visual (with shaders)

---
+++ Assets/_Project/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,19 @@
+﻿using _Project.Scripts.Kitchen;
+using Project.Player;
+
+namespace _Project.Scripts.Counters
+{
+    public class DeliveryCounter : BaseCounter
+    {
+        public override void Interact(PlayerController player)
+        {
+            if (player.HasKitchenObject())
+            {
+                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plate))
+                {
+                    player.GetKitchenObject().DestroySelf();
+                }
+            }
+        }
+    }
+}
