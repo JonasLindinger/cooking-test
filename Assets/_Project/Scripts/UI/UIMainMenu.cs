c2420ce1b865f2de9dbe7b86111178416898629f
Jonas <lindinger.jonas.yt@gmail.com>
2025-08-01 16:19:15 +0200

    add: basic main menu

---
+++ Assets/_Project/Scripts/UI/UIMainMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace _Project.Scripts.UI
+{
+    public class UIMainMenu : MonoBehaviour
+    {
+        [SerializeField] private Button playButton;
+        [SerializeField] private Button quitButton;
+
+        private void Awake()
+        {
+            playButton.onClick.AddListener(() =>
+            {
+                // Click
+                SceneManager.LoadScene(1);
+            });
+            
+            quitButton.onClick.AddListener(() =>
+            {
+                // Quit
+                Application.Quit();
+            });
+        }
+    }
+}
